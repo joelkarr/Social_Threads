19d82adc9bb59612566ba97e9800580b6c0f0073
Joel Karr <joelkarr@social-threads.com>
2012-07-24 21:55:21 -0500

    add markup for view cart page

---
+++ SOC.Web/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using SOC.Web.Models.ViewModels.Base;
+
+namespace SOC.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        //
+        // GET: /Checkout/
+
+        public ActionResult Current()
+        {
+
+            return View("Current", new BaseViewModel());
+        }
+
+
+    }
+}
--- SOC.Web/Controllers/OrderController.cs
+++ SOC.Web/Controllers/OrderController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using SOC.Entities;
+﻿using System.Web.Mvc;
 using SOC.Web.Models.ViewModels.Base;
 
 namespace SOC.Web.Controllers
